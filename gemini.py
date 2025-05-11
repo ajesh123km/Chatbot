f68dc270142f14d494508791398d8de983510122
AJESH <158215604+ajesh123km@users.noreply.github.com>
2025-05-12 00:18:52 +0530

    Update gemini.py

--- gemini.py
+++ gemini.py
@@ -6,7 +6,7 @@ genai.configure(api_key="AIzaSyBarZkGOiou8V7uXPkZtRuTd0tXwIEKJFU")
 # Initialize the model
 model = genai.GenerativeModel("gemini-2.0-flash")  # Make sure the model name is correct
 
-# Generate content
+# Generate content 
 response = model.generate_content("hi how are you")
 
 # Print the response text
